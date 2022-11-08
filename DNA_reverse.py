0e0a532543c91ec7c749d86795c670ca25b8cfc4
WesleyL168 <wesleylim168@gmail.com>
2022-11-08 17:20:41 -0500

    commit 11/8

--- DNA_reverse.py
+++ DNA_reverse.py
@@ -1,9 +1,13 @@
 dna = input("Please provide template: ")
 dna = dna.upper()
 
+def reversed(dna):
+    return dna[::-1]
+reversed_dna = reversed(dna)
+
 def complement(dna):
     complement = ""                 #creates blank complement
-    for base in dna:
+    for base in reversed_dna:
         if base == "A":             # A to T
             complement += "T"
         elif base == "T":           # T to A
@@ -13,4 +17,5 @@ def complement(dna):
         elif base == "G":           # G to C
             complement += "C"
     return complement
-print(complement(dna))
+print("Reversed complement DNA (5' to 3'): " , complement(dna))
+
